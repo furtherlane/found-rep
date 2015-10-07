201c7054b05f40c3efd3aadef57592d2d24c9799
Steven C. Nathanson <steve@Stevens-Mac-Pro.local>
2015-10-07 17:19:40 -0400

    Initial Menu Design

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(".tab-bar .menu-icon").click(function() {
+	$(this).toggleClass( "open" );
+	$("#menu-icon-section").toggleClass("open");
+});
