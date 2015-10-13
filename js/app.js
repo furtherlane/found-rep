de83e7a81e257e43ee37225126dbdca2a2db9912
Steven C. Nathanson <steve@Stevens-Mac-Pro.local>
2015-10-13 10:47:28 -0400

    black search bar version

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@ $(".tab-bar .menu-icon").click(function() {
 	$(this).toggleClass( "open" );
 	$("#menu-icon-section").toggleClass("open");
 });
+
+$("#search-button").click(function(){
+//	$(this).toggleClass("open");
+//	$(".search-section").toggleClass("open");
+//	$(".search-section").slideToggle("fast", "linear");
+	$(".search-section").toggle();
+	$("input[type='text'].search-bar").focus();
+})
